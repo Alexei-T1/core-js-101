9693d2c2508d4a5bb35fb5b2cc424e40c571fc4b
Alexei-T1 <turencko.alexei@yandex.ru>
2022-01-18 18:23:20 +0400

    add solution date task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,16 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4 !== 0) {
+    return false;
+  } if (year % 100 !== 0) {
+    return true;
+  } if (year % 400 !== 0) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -73,8 +81,25 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let milS = new Date(endDate) - new Date(startDate);
+  const hour = 60 * 60 * 1000;
+  const hourGetCount = milS / hour > 0 ? Math.floor(milS / hour) : 0;
+  const hourCount = hourGetCount < 10 ? `0${hourGetCount}` : hourGetCount;
+  milS -= hourGetCount * hour;
+  const min = 60 * 1000;
+  const minGetCount = milS / min > 0 ? Math.floor(milS / min) : 0;
+  const minCount = minGetCount < 10 ? `0${minGetCount}` : minGetCount;
+  milS -= minGetCount * min;
+  const sec = 1000;
+  const secGetCount = milS / sec > 0 ? Math.floor(milS / sec) : 0;
+  const secCount = secGetCount < 10 ? `0${secGetCount}` : secGetCount;
+  milS -= secGetCount * sec;
+  if (milS < 100) {
+    milS = milS > 9 ? `0${milS}` : `00${milS}`;
+  }
+
+  return `${hourCount}:${minCount}:${secCount}.${milS}`;
 }
 
 
@@ -94,8 +119,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const dateO = new Date(date);
+  const min = dateO.getUTCMinutes();
+  const hour = dateO.getUTCHours() > 12 ? dateO.getUTCHours() - 12 : dateO.getUTCHours();
+  const hourDeg = 0.5 * (60 * hour + min);
+  const minDeg = 6 * min;
+  let deg = Math.abs(hourDeg - minDeg);
+  deg = deg > 180 ? deg - 180 : deg;
+  return deg * (Math.PI / 180);
 }
 
 
